bb837604e87038da89661c07e322ffeeb5e0dae7
DFOLEY <DFOLEY>
2017-09-22 15:40:17 -0600

    Added header logic

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ var app = express()
 app.use(bodyParser.json())
 app.use(express.static(__dirname))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', function(req, res) {
 	res.send('index.html')
 })
